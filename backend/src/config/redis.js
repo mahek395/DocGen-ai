1ac8c5aab83aa4bb04e7f3875c0acd9fa608d1d2
Mahek Shah <mahekshah395@gmail.com>
2026-03-19 20:28:21 +0530

    fix redis auth

--- backend/src/config/redis.js
+++ backend/src/config/redis.js
@@ -6,6 +6,7 @@ dotenv.config();
 const redisConnection = new IORedis({
   host: process.env.REDIS_HOST,
   port: process.env.REDIS_PORT,
+  password: process.env.REDIS_PASSWORD,
   maxRetriesPerRequest: null
 });
 
