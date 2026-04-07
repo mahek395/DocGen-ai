94a825043925451b34bae14d52792fa539701648
Mahek Shah <mahekshah395@gmail.com>
2026-04-07 18:45:47 +0530

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,38 +4,35 @@ import jobRoutes from "./routes/job.routes.js";
 import "./config/redis.js";
 import cors from "cors";
 
-
-// Load environment variables
 dotenv.config();
 
 const app = express();
+
 const allowedOrigins = [
   "http://localhost:5173",
   "https://doc-gen-ai-u1v5.vercel.app",
-  "https://doc-gen-ai-u1v5-dl41y4rtk-mahekshah395-8301s-projects.vercel.app"
+  "https://doc-gen-ai-u1v5-dl41y4rtk-mahekshah395-8301s-projects.vercel.app",
+  "https://doc-gen-ai-u1v5-e63qwpjmd-mahekshah395-8301s-projects.vercel.app"
 ];
 
 app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-  methods: ["GET", "POST"],
+  origin: allowedOrigins,
+  methods: ["GET", "POST", "OPTIONS"],
+  allowedHeaders: ["Content-Type"],
   credentials: true
 }));
 
+// handle preflight requests
+app.options("*", cors());
+
 app.use(express.json());
 
-// Health check route (very useful)
+// Health check
 app.get("/health", (req, res) => {
   res.json({ status: "OK", message: "Server is running" });
 });
 
-// Job-related routes
+// Routes
 app.use("/api/jobs", jobRoutes);
-
 
 export default app;
